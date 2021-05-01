b9ee610cb5576d54f2fe1830544edb8065b43a2d
Skoropad Aleksandr <askoropad@solit-clouds.ru>
2021-05-01 16:19:17 +0300

    feat(project): build test

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ const verifyConditions = require('./lib/verifyConditions');
 const success = require('./lib/success');
 const fail = require('./lib/fail');
 
-const TelegramBot = require('node-telegram-bot-api');
-
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '1243432139:AAHzn1JwNazvE0n3a8GwliSGMW4UgfqKQ30';
-
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
-
-bot.sendMessage('408225585', 'Received your mas*dasd essage').then(() => console.log('done'));
-
 module.exports = {
 	verifyConditions,
 	success,
