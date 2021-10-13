4d0d6ffa66a99a7ccca46dd241e66b4e995f1066
Skoropad Aleksandr <askoropad@solit-clouds.ru>
2021-10-13 11:45:02 +0300

    test(project): added tests

---
+++ tests/render-message.spec.ts
@@ -0,0 +1,62 @@
+import {renderMessage} from '../src/common/render-message';
+import {TGBotRenderedMessage} from '../src/interfaces/rendered-message';
+
+describe('Render message', () => {
+	describe('Markdown', () => {
+		it('Message should render correctly', async () => {
+			const renderedMessage: TGBotRenderedMessage = renderMessage({message: '**Title**\nContent'});
+			expect(renderedMessage.message).toBe('*Title*\nContent');
+			expect(renderedMessage.format).toBe('markdown');
+		});
+
+		it('Message should render context correctly', async () => {
+			const renderedMessage: TGBotRenderedMessage = renderMessage({message: '**${title}**\n${content}'}, {title: 'Title', content: 'Content'});
+			expect(renderedMessage.message).toBe('*Title*\nContent');
+			expect(renderedMessage.format).toBe('markdown');
+		});
+	})
+
+	describe('HTML', () => {
+		it('Message should render correctly', async () => {
+			const renderedMessage: TGBotRenderedMessage = renderMessage({message: '<b>Title</b>\nContent', format: 'html'});
+			expect(renderedMessage.message).toBe('<b>Title</b>\nContent');
+			expect(renderedMessage.format).toBe('html');
+		});
+
+		it('Message should render context correctly', async () => {
+			const renderedMessage: TGBotRenderedMessage = renderMessage({message: '<b>${title}</b>\n${content}', format: 'html'}, {title: 'Title', content: 'Content'});
+			expect(renderedMessage.message).toBe('<b>Title</b>\nContent');
+			expect(renderedMessage.format).toBe('html');
+		});
+	})
+});
+
+describe('Render template message', () => {
+	describe('Markdown', () => {
+		it('Message should render correctly', async () => {
+			const renderedMessage: TGBotRenderedMessage = renderMessage({path: './tests/common/message.md'});
+			expect(renderedMessage.message).toBe('*Title*\r\nContent');
+			expect(renderedMessage.format).toBe('markdown');
+		});
+
+		it('Message should render context correctly', async () => {
+			const renderedMessage: TGBotRenderedMessage = renderMessage({path: './tests/common/message-context.md'}, {title: 'Title', content: 'Content'});
+			expect(renderedMessage.message).toBe('*Title*\r\nContent');
+			expect(renderedMessage.format).toBe('markdown');
+		});
+	})
+
+	describe('HTML', () => {
+		it('Message should render correctly', async () => {
+			const renderedMessage: TGBotRenderedMessage = renderMessage({path: './tests/common/message.html'});
+			expect(renderedMessage.message).toBe('<b>Title</b>\r\nContent');
+			expect(renderedMessage.format).toBe('html');
+		});
+
+		it('Message should render context correctly', async () => {
+			const renderedMessage: TGBotRenderedMessage = renderMessage({path: './tests/common/message-context.html'}, {title: 'Title', content: 'Content'});
+			expect(renderedMessage.message).toBe('<b>Title</b>\r\nContent');
+			expect(renderedMessage.format).toBe('html');
+		});
+	})
+});
